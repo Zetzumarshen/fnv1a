29a4de24eb91df9952f109cd5a8f036d74c3b77e
Zetzumarshen <Zetzumarshen@gmail.com>
2015-03-12 02:17:34 +0700

    Delete fnv1.hs

--- fnv1.hs
+++
@@ -1,7 +0,0 @@
-import Data.Char (ord)
-import Data.Bits (xor,(.&.))
-
-fnv1a x = let initHash = 2166136261
-          in  foldl fnv1a_Iter initHash x
-fnv1a_Iter hash x = let fnvPrime = 16777619
-                    in  (hash `xor` (ord x .&. 0xff)) * fnvPrime
